49f38bca9eb7d7e1d22128f5bf8ab707ba7dac10
Jimmy Yau <personal@takkan.org>
2025-03-09 15:37:53 +1100

    added int main

---
+++ comp1511/custom_cake.c
@@ -0,0 +1,17 @@
+//
+//  custom_cake.c
+//  
+//
+//  Created by Jimmy Yau on 9/3/2025.
+//
+
+#include <stdio.h>
+
+
+int main(void){
+    
+    printf("Enter layers: ");
+    
+    
+    return 0;
+}
---
+++ comp1511/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Comp1511
+//
+//  Created by Jimmy Yau on 9/3/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
